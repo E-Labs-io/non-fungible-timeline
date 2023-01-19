572184a5ac865002350f3a27dce93b45c9e84cb1
e-labs-io <hello@e-labs.io>
2023-01-19 22:57:25 +0000

    fixed build issue

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,21 +1,27 @@
+/** @format */
+
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 import React from "react";
 import styled from "styled-components";
 
 import Header from "./Header/Header";
 
 const Main = styled.main`
-	font-family: "Ubuntu", sans-serif;
-	box-sizing: border-box;
+  font-family: "Ubuntu", sans-serif;
+  box-sizing: border-box;
 `;
 
 const Layout: NextPage = ({ children }) => {
-	return (
-		<>
-			<Header />
-			<Main>{children}</Main>
-		</>
-	);
+  const router = useRouter();
+
+  const onBack = () => router.push("/");
+  return (
+    <>
+      <Header onBack={onBack} />
+      <Main>{children}</Main>
+    </>
+  );
 };
 
 export default Layout;
