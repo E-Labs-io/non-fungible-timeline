6bb77e303d159102140e01f24ec5fbfaeeb3f6cb
e-labs-io <hello@e-labs.io>
2023-01-19 22:56:05 +0000

    added FAQ page

--- components/common/TextExpandList.tsx
+++ components/common/TextExpandList.tsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import React, { Fragment } from "react";
 import styled, { useTheme } from "styled-components";
 
@@ -6,50 +8,44 @@ import TextExpand from "./TextExpand";
 import { device } from "config/media";
 
 const Wrapper = styled.dl`
-	width: 100%;
-	max-width: 70vw;
-	display: flex;
-	flex-direction: column;
-	font-size: ${({ theme }) => theme.fontSizes.medium};
-	text-align: left;
+  width: 100%;
+  max-width: 70vw;
+  display: flex;
+  flex-direction: column;
+  font-size: ${({ theme }) => theme.fontSizes.medium};
+  text-align: left;
+  box-shadow: inset 0px 0px 15px 2px rgba(207, 207, 207, 0.682);
+  border-radius: 20px;
+  padding: 20px;
+  row-gap: 20px;
 
-	@media ${device.tablet} {
-		max-width: 80vw;
-	}
+  @media ${device.tablet} {
+    max-width: 80vw;
+  }
 
-	@media ${device.mobileL} {
-		max-width: 90vw;
-	}
+  @media ${device.mobileL} {
+    max-width: 90vw;
+  }
 `;
 
 interface TextExpandListProps {
-	data: { title: string; content?: string }[];
+  data: { title: string; content?: string }[];
 }
 
 function TextExpandList({ data }: TextExpandListProps) {
-	const theme = useTheme();
-	const dataLength = data && data.length;
-	return (
-		<Wrapper>
-			{data.length
-				? data.map((item, index) => (
-						<Fragment key={index}>
-							<TextExpand
-								title={item.title}
-								content={item?.content}
-							/>
-							{index + 1 < dataLength ? (
-								<Divider
-									borderColor={theme.primaryLight}
-									borderWidth="1px"
-									margin="12px 0"
-								/>
-							) : null}
-						</Fragment>
-				  ))
-				: null}
-		</Wrapper>
-	);
+  const theme = useTheme();
+  const dataLength = data && data.length;
+  return (
+    <Wrapper>
+      {data.length
+        ? data.map((item, index) => (
+            <Fragment key={index}>
+              <TextExpand title={item.title} content={item?.content} />
+            </Fragment>
+          ))
+        : null}
+    </Wrapper>
+  );
 }
 
 export default TextExpandList;
---
+++ constants/faqQuestions.ts
@@ -0,0 +1,26 @@
+/** @format */
+
+const faqQuestions = [
+  {
+    title: "What is the Non-Fungible Timeline?",
+    content:
+      "degenXchange is a trustless 1 to 1 randomise.  NFT swap/. You enter an old NFT you don’t want anymore, and then when you trigger a draw, it will automatically pick a random NFT from the pool and transfers it to you.",
+  },
+  {
+    title: "What is the Non-Fungible Timeline?",
+    content:
+      "degenXchange is a trustless 1 to 1 randomise.  NFT swap/. You enter an old NFT you don’t want anymore, and then when you trigger a draw, it will automatically pick a random NFT from the pool and transfers it to you.",
+  },
+  {
+    title: "What is the Non-Fungible Timeline?",
+    content:
+      "degenXchange is a trustless 1 to 1 randomise.  NFT swap/. You enter an old NFT you don’t want anymore, and then when you trigger a draw, it will automatically pick a random NFT from the pool and transfers it to you.",
+  },
+  {
+    title: "What is the Non-Fungible Timeline?",
+    content:
+      "degenXchange is a trustless 1 to 1 randomise.  NFT swap/. You enter an old NFT you don’t want anymore, and then when you trigger a draw, it will automatically pick a random NFT from the pool and transfers it to you.",
+  },
+];
+
+export default faqQuestions;
