c99a71dcdb0aec19c2d8699af6f28871033092f5
e-labs-io <hello@e-labs.io>
2023-01-26 16:51:54 +0000

    updated some FAQs

--- constants/faqQuestions.ts
+++ constants/faqQuestions.ts
@@ -11,6 +11,11 @@ const faqQuestions = [
     content: "Most of them! All ERC721 & ERC1155 on the Ethereum mainnet.",
   },
   {
+    title: "How do you classify a 'verified' project?",
+    content:
+      "We use BlockDaemons verified contract API, they have over 2000 verified projects.",
+  },
+  {
     title: "Will you show other chains??",
     content:
       "Of course! First of all we will integrate other EVM based chains.",
@@ -19,6 +24,11 @@ const faqQuestions = [
     title: "What else can you do?",
     content:
       "Right now, display the rich history of a wallet's journey though the NFT universe. We have many plans to enrich your experience and bring useful tools.  One of witch, you will be able to vote on a wallet in given category's!",
+  },
+  {
+    title: "Your UI & Social media sucks, how can i get involved?",
+    content:
+      "Well your not wrong there, hit us up on twitter if you think you can bring something to this project.",
   },
 ];
 
